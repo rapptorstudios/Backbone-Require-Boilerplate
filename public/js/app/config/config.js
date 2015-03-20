7c25b137490e09bdd8041043df8493f3ec0e1cb7
Whitney Williams <wweliz@gmail.com>
2015-03-19 22:30:44 -0400

    adding bootstrap

--- public/js/app/config/config.js
+++ public/js/app/config/config.js
@@ -6,13 +6,16 @@ require.config({
 		'jquery': 						'../libs/jquery/dist/jquery',
 		'underscore': 				'../libs/underscore/underscore',
 		'backbone': 					'../libs/backbone/backbone',
-		'text': 							'../libs/requirejs-text/text'
+		'text': 							'../libs/requirejs-text/text',
+		'bootstrap': 					'../libs/components-bootstrap/js/bootstrap.min' 
 		// 'foundation.core': 		'../libs/foundation/js/foundation',
-		// 'foundation.topbar': 	'../libs/foundation/js/foundation/foundation.topbar',
+		// 'foundation.topbar': 	'../libs/foundation/js/foundation/foundation.topbar'
 		// 'modernizr': 					'../libs/foundation/js/vendor/modernizr'
 	},
 
 	shim: {
+
+		'bootstrap': { deps:['jquery'] } 
 		// 'foundation.core': {
 		// 		deps: [
 		// 		'jquery',
